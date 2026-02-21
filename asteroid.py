e36a5ab98c8c807920fcb2b9e85f1f5ae111152a
GwynneMichele <gwynnemontgomery@gmail.com>
2026-02-21 18:57:05 -0500

    Evening commit 2/21/2016

---
+++ asteroid.py
@@ -0,0 +1,26 @@
+# import dependencies
+import pygame
+
+from circleshape import CircleShape
+from constants import LINE_WIDTH
+
+# define class
+class Asteroid(CircleShape):
+    containers = ()     
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen,
+            "white",
+            (int(self.position.x), int(self.position.y)),
+            int(self.radius),
+            LINE_WIDTH
+            )
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
+    
