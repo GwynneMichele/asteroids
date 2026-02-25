e2ac3cf4ad8ec3afa42a796ecd6747c100c63b73
GwynneMichele <gwynnemontgomery@gmail.com>
2026-02-25 16:01:53 -0500

    Working Game Commit 2/25/2026

--- asteroid.py
+++ asteroid.py
@@ -1,26 +1,37 @@
-# import dependencies
+import random
+
 import pygame
-
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
+from logger import log_event
 
-# define class
+
 class Asteroid(CircleShape):
-    containers = ()     
-
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(
-            screen,
-            "white",
-            (int(self.position.x), int(self.position.y)),
-            int(self.radius),
-            LINE_WIDTH
-            )
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
     
--- main.py
+++ main.py
@@ -49,6 +49,12 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
         screen.fill("black")
 
         # Drawable loop
